7b163f3eabc51ba3d5cdb2fb3e22db7cdd39d9c6
Peterson Larson <peter-larson@hotmail.com>
2023-08-21 23:19:43 -0300

    tests_from_probs

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <random>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 static bool gameStatus = false;
 static bool gameWon = false;
@@ -61,20 +62,116 @@ float gameProbs() {
     return multi;
 }
 
+float myTurn() {
+    std::srand(static_cast<unsigned int>(time(0)));
+
+    std::vector<float> probabilities = {0.008f, 0.02f, 0.05f, 0.1f, 0.222f, 0.60f};
+
+    int randomInt = randomWithProbabilities(probabilities);
+
+    float multi = 0.0f;
+    float randomFloat = 0.0f;
+
+    if (randomInt == 0) {
+        multi = static_cast<float>(std::rand() % (1000 - 4192 + 1) + 4192) / 100.0f;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100.0f;
+    }
+    else if (randomInt == 1) {
+        multi = static_cast<float>(std::rand() % (4191 - 867 + 1) + 867) / 100.0f;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100.0f;
+    }
+    else if (randomInt == 2) {
+        multi = static_cast<float>(std::rand() % (866 - 195 + 1) + 195) / 100.0f;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100.0f;
+    }
+    else if (randomInt == 3) {
+        multi = static_cast<float>(std::rand() % (194 - 140 + 1) + 140) / 100.0f;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100.0f;
+    }
+    else if (randomInt == 4) {
+        multi = static_cast<float>(std::rand() % (139 - 124 + 1) + 124) / 100.0f;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100.0f;
+    }
+    else if (randomInt == 5) {
+        multi = static_cast<float>(std::rand() % (124 - 1 + 1) + 1) / 100.0f;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100.0f;
+    }
+
+    if (multi < 1) {
+        multi = multi + 1;
+    }
+
+    std::cout << "Random Int: " << randomInt << std::endl;
+    std::cout << "Multiplier: " << multi << std::endl;
+
+    return multi;
+}
+
+float impossibleProb() {
+    std::srand(static_cast<unsigned int>(time(0)));
+
+    std::vector<float> probabilities = {0.008f, 0.02f, 0.05f, 0.1f, 0.222f, 0.60f};
+
+    int randomInt = randomWithProbabilities(probabilities);
+
+    float multi = 0.0f;
+    float randomFloat = 0.0f;
+
+    if (randomInt == 0) {
+        multi = static_cast<float>(std::rand() % static_cast<int>(std::round((1000 - 41.92) * 100))) / 100 + 41.92;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100;
+    }
+    else if (randomInt == 1) {
+        multi = static_cast<float>(std::rand() % static_cast<int>(std::round((41.91 - 8.67) * 100))) / 100 + 8.67;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100;
+    }
+    else if (randomInt == 2) {
+        multi = static_cast<float>(std::rand() % static_cast<int>(std::round((8.66 - 1.95) * 100))) / 100 + 1.95;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100;
+    }
+    else if (randomInt == 3) {
+        multi = static_cast<float>(std::rand() % static_cast<int>(std::round((1.94 - 1.40) * 100))) / 100 + 1.40;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100;
+    }
+    else if (randomInt == 4) {
+        multi = static_cast<float>(std::rand() % static_cast<int>(std::round((1.39 - 1.24) * 100))) / 100 + 1.24;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100;
+    }
+    else if (randomInt == 5) {
+        multi = static_cast<float>(std::rand() % static_cast<int>(std::round((1.24 - 1) * 100))) / 100 + 1.0;
+        randomFloat = static_cast<float>(std::rand()) / RAND_MAX;
+        multi = std::floor((multi + randomFloat) * 100) / 100;
+    }
+
+    std::cout << "Random Int: " << randomInt << std::endl;
+    std::cout << "Multiplier: " << multi << std::endl;
+
+    return multi;
+}
+
 void startGame(int bet, float multiplicator) {
-    float multi = gameProbs();
+    multi = myTurn();
     gameStatus = true;
 
     if (multiplicator <= multi) {
-        betWon = bet * multiplicator;
+        betWon = (bet * multiplicator) - bet;
         gameWon = true;
     }
     else {
         gameWon = false;
     }
-
 }
-
 
 int main() {
     // Inicialização do GLFW
@@ -82,7 +179,7 @@ int main() {
         return 1;
 
     // Configuração da janela do GLFW
-    GLFWwindow *window = glfwCreateWindow(800, 600, "ImGui Input e Cálculo", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "ImGui Input Probabilistico", nullptr, nullptr);
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
@@ -114,9 +211,11 @@ int main() {
 
         if (gameStatus && gameWon) {
             ImGui::Text("Você ganhou %.2f", betWon);
+            ImGui::Text("Multiplicador %.2f", multi);
         }
         if (gameStatus && !gameWon) {
-            ImGui::Text("Você perdeu");
+            ImGui::Text("Você perdeu %d", bet);
+            ImGui::Text("Multiplicador %.2f", multi);
         }
 
         // Renderização da GUI do Dear ImGui
